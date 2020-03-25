a60d046dbdd821c4ef76a2391588a15651bf223e
Olatayo Abdul-Azeez <hayzed003@gmail.com>
2020-03-25 21:53:38 +0100

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,14 @@
+import math
+print ('Welcome User')
+print ('This program accepts the radius of a circle and returns the area as an output')
+print('--------------------------------')
+print('Please input radius')
+radius = float(input())
+Area = math.pi * radius * radius
+print('Calculating Area of the Circle, wait a minute!')
+print('--------------------------------')
+print('Area of Circle is: ')
+print(Area)
+print('--------------------------------')
+print('No need to try using a calculator, the answer is spot on')
+print('Thank you')
